d5bbbe61783d36a7279733ed0d6ef6b27ec5dbc2
eslamicoo912 <eslamicoo3@gmail.com>
2023-10-15 10:37:54 +0300

    creating handler and options

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,31 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "username", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch("http://localhost:3000/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials),
+        });
+        const user = await res.json();
+        if (res.ok && user) return user;
+        return null;
+      },
+    }),
+  ],
+};
